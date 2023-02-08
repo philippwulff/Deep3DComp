f9abac0a77900dcbb50cb7f883ff8b1d5371a394
Philipp Wulff <philipp.sylt@gmail.com>
2023-02-08 16:51:47 +0100

    finished hparams grid search

--- hparams_grid_search.py
+++ hparams_grid_search.py
@@ -7,8 +7,14 @@ import deep_sdf
 from math import log10
 from collections.abc import MutableMapping
 from itertools import product
+import argparse
+import copy
+import re
+import logging
 
-def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str ='.') -> MutableMapping:
+
+def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str = '.') -> MutableMapping:
+    """Helper function"""
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
@@ -18,72 +24,112 @@ def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str ='.') -> Muta
             items.append((new_key, v))
     return dict(items)
 
+
+# Define default hparams.
 SEARCH_DIR = "/home/shared/deepsdfcomp/searches/siren_500_gridsearch_quarter_params"
 DEFAULT_SPECS_FILE = "/home/shared/deepsdfcomp/searches/siren_500_gridsearch_quarter_params/default_specs.json"
 
-import argparse
+# Define hparams search options.
+GRID_AXES = {
+    "NetworkSpecs": {
+        "norm_layers": [[],],
+        "dropout": [[],],
+        "dropout_prob" : [0,],
+        "latent_in" : [[], [4]],
+        # "xyz_in_all": [True, False],
+        "latent_dropout" : [True, False],
+    },
+    # 1st lr schedule is for network and 2nd lr schedule is for latents
+    "LearningRateSchedule": [       
+        [{
+            "Type": "Step",
+            "Initial": 0.0005,
+            "Interval": 500,
+            "Factor": 0.5
+        },
+        {
+            "Type": "Step",
+            "Initial": 0.001,
+            "Interval": 500,
+            "Factor": 0.5
+        }],
+        [{
+            "Type": "Step",
+            "Initial": 0.005,
+            "Interval": 500,
+            "Factor": 0.5
+        },
+        {
+            "Type": "Step",
+            "Initial": 0.01,
+            "Interval": 500,
+            "Factor": 0.5
+        }],
+    ],
+    "CodeLength" : [16, 200],
+    "CodeRegularization" : [True, False],
+}
 
-arg_parser = argparse.ArgumentParser(description="Train a DeepSDF autodecoder")
-deep_sdf.add_common_args(arg_parser)
-args = arg_parser.parse_args()
-deep_sdf.configure_logging(args)
+if __name__ == "__main__":
 
-exps = []
-grid_axes = {"NetworkSpecs": {"norm_layers": [[],],
-                              "dropout": [[],],
-                              "dropout_prob" : [0,],
-                              "latent_in" : [[], [4]],
-                              "xyz_in_all": [True], # maybe remove
-                              "latent_dropout" : [True, False],},
-             "LearningRateSchedule": {[
-             ]},
-             "CodeLength" : [10, 256],
-             "CodeRegularization" : [True, False],
-            }
+    arg_parser = argparse.ArgumentParser(description="Hyperparameter Grid Search")
+    deep_sdf.add_common_args(arg_parser)
+    args = arg_parser.parse_args()
+    deep_sdf.configure_logging(args)
 
-network_specs_list = list(dict(zip(grid_axes["NetworkSpecs"].keys(), values)) 
-                          for values in product(*grid_axes["NetworkSpecs"].values()))
-grid_axes_copy = {**grid_axes}; 
-if "NetworkSpecs" in grid_axes_copy.keys():
-    del grid_axes_copy["NetworkSpecs"]
-root_list = list(dict(zip(grid_axes_copy.keys(), values)) 
-                          for values in product(*grid_axes_copy.values()))
-exps_separate = list(product(network_specs_list, root_list))
-for exp_separate in exps_separate:
-    exps.append({"NetworkSpecs": exp_separate[0], ** exp_separate[1]})
-
+    # Get all possible combinations of NetworkSpecs.
+    network_specs_permutations = list(dict(zip(GRID_AXES["NetworkSpecs"].keys(), values)) 
+                            for values in product(*GRID_AXES["NetworkSpecs"].values()))
     
-# read data
-with open(DEFAULT_SPECS_FILE) as f:
-    default_specs = json.load(f)
+    # Get all possible combinations of the other options.
+    grid_axes_without_network_specs = {k: v for k, v in GRID_AXES.items() if k != "NetworkSpecs"}
+    other_permutations = list(dict(zip(grid_axes_without_network_specs.keys(), values)) 
+                            for values in product(*grid_axes_without_network_specs.values()))
 
-codelengths = [4, 32, 128, 256]
-for exp in exps:
-    specs = {**default_specs}
-    for k, v in exp.items():
-        if type(v) == dict:
-            for kk, vv in v.items():
-                specs[k][kk] = vv
+    # Combine all combinations and generate experiment hparams list.
+    all_permutations = list(product(network_specs_permutations, other_permutations))
+    exps = []
+    for perm in all_permutations:
+        exps.append({
+            "NetworkSpecs": perm[0], 
+            **perm[1]
+        })
+        
+    # Read default specifications in order to override them.
+    with open(DEFAULT_SPECS_FILE) as f:
+        default_specs = json.load(f)
+
+    for exp in exps:
+        # Produce specs for this experiment.
+        specs = copy.deepcopy(default_specs)
+        for k, v in exp.items():
+            if isinstance(v, dict):
+                for kk, vv in v.items():
+                    specs[k][kk] = vv
+                continue
+            specs[k] = v
+
+        # Get exp name (counter + searched hparams).
+        exp_number = 0
+        if os.path.exists(SEARCH_DIR):
+            dir_names = [name for name in os.listdir(SEARCH_DIR) if os.path.isdir(os.path.join(SEARCH_DIR, name))]
+            exp_nos = [int(name.split("_")[1]) for name in dir_names]
+            if len(exp_nos)>0:
+                exp_number = max(exp_nos) + 1
+        exp_name = f"exp_{exp_number:04}"
+        
+        searched_hparams = flatten_dict(exp)
+        if len(searched_hparams) > 0:
+            exp_name += "_" + "_".join(f"{k}={round(v, -int(log10(abs(v)))+3)}" if isinstance(v, float) else f"{k}={v}" for k, v in searched_hparams.items() if v)
+        # Create exp directory and write specs.json
+        exp_dir = os.path.join(SEARCH_DIR, exp_name)
+        if not os.path.exists(exp_dir):
+            os.makedirs(exp_dir)
+        else:
             continue
-        specs[k] = v
-
-    # find exp name (continuous counter and searched hparams)
-    exp_number = 0
-    if os.path.exists(SEARCH_DIR):
-        dir_names = [name for name in os.listdir(SEARCH_DIR) if os.path.isdir(os.path.join(SEARCH_DIR, name))]
-        exp_nos = [int(name.split("_")[1]) for name in dir_names]
-        if len(exp_nos)>0:
-            exp_number = max(exp_nos) + 1
-    exp_name = f"exp_{exp_number:04}"
-    
-    # create exp directory and write specs.json
-    searched_hparams = flatten_dict(exp)
-    if len(searched_hparams) > 0:
-        exp_name += "_" + "_".join(f"{k}={round(v, -int(log10(abs(v)))+3)}" if isinstance(v, float) else f"{k}={v}" for k, v in searched_hparams.items())
-    exp_dir = os.path.join(SEARCH_DIR, exp_name)
-    os.makedirs(exp_dir)
-    with open(os.path.join(exp_dir, "specs.json"), "w+") as f:
-        json.dump(specs, f, indent=4)
-    
-    # start experiment
-    main_function(exp_dir, None, 3)
+        with open(os.path.join(exp_dir, "specs.json"), "w+") as f:
+            json.dump(specs, f, indent=4)
+        
+        # Start experiment
+        logging.info(f"STARTING EXP: {exp_name}")
+        main_function(exp_dir, continue_from=None, batch_split=3)     
