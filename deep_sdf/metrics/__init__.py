7bcfa73d9af11e6df2a154c81c5807f28cb46d7f
Philipp Wulff <philipp.sylt@gmail.com>
2023-02-22 12:27:59 +0100

    added mesh comparison plot

--- deep_sdf/metrics/__init__.py
+++ deep_sdf/metrics/__init__.py
@@ -1,16 +1,20 @@
 import trimesh
 from deep_sdf.metrics.chamfer import compute_chamfer
-from deep_sdf.utils import as_mesh
+from deep_sdf.utils import as_mesh, trimesh_to_pytorch3d_meshes
+from pytorch3d.loss import mesh_normal_consistency
 
 
-def compute_metric(gt_mesh, gen_mesh, num_mesh_samples=30000, metric="chamfer"):
-    if isinstance(gt_mesh, str):
+def compute_metric(gt_mesh=None, gen_mesh=None, num_mesh_samples=30000, metric="chamfer"):
+    if gt_mesh is not None and isinstance(gt_mesh, str):
         gt_mesh = as_mesh(trimesh.load_mesh(gt_mesh))
-    if isinstance(gen_mesh, str):
+    if gen_mesh is not None and isinstance(gen_mesh, str):
         gen_mesh = as_mesh(trimesh.load_mesh(gen_mesh))
-    gen_points_sampled = trimesh.sample.sample_surface(gen_mesh, num_mesh_samples)[0]
-    gt_points_sampled = trimesh.sample.sample_surface(gt_mesh, num_mesh_samples)[0]
-    if metric == "chamfer":
+    if metric == "chamfer" and gt_mesh is not None and gen_mesh is not None:
+        gen_points_sampled = trimesh.sample.sample_surface(gen_mesh, num_mesh_samples)[0]
+        gt_points_sampled = trimesh.sample.sample_surface(gt_mesh, num_mesh_samples)[0]
         return compute_chamfer(gen_points_sampled, gt_points_sampled)
+    elif metric == "normal_consistency":
+        meshes = trimesh_to_pytorch3d_meshes([gen_mesh])
+        return mesh_normal_consistency(meshes)
     else:
         return NotImplementedError(f"Chosen metric '{metric}' does not exist.")
--- deep_sdf/plotting.py
+++ deep_sdf/plotting.py
@@ -2,7 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import logging
 import time
-from typing import Union
+from typing import Union, List
+import os
+import deep_sdf.workspace as ws
+from deep_sdf import utils, metrics
+import trimesh
+import pyrender
+import math
+import pandas as pd
+
+
+os.environ['PYOPENGL_PLATFORM'] = 'egl'
 
 
 def plot_train_stats(loss_hists: list, psnr_hist=None, step_hist=None, labels=None, save_path="") -> plt.figure:
@@ -75,3 +85,97 @@ def plot_dist_violin(data: np.ndarray, percentile_keys: list=[50, 75, 90, 99]) -
     logging.debug(f"Plotting all chamfer distances took {time.time()-start}sec")
 
     return fig, percentiles
+
+
+def pyrender_helper(mesh: trimesh.Trimesh, alpha=0, beta=0, gamma=0):
+    """Renders a Trimesh and returns the color and depth image numpy arrays."""
+    mesh = pyrender.Mesh.from_trimesh(mesh)
+    scene = pyrender.Scene()
+    scene.add(mesh)
+    camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0, aspectRatio=1.0)
+    camera_pose = np.eye(4)
+    camera_pose[2, 3] = 2      # move in z-dir
+    camera_pose = utils.rotate(camera_pose, alpha=alpha, beta=beta, gamma=gamma)
+    scene.add(camera, pose=camera_pose)
+    # light = pyrender.SpotLight(color=np.ones(3), intensity=3.0,
+    #                             innerConeAngle=np.pi/16.0,
+    #                             outerConeAngle=np.pi/6.0)
+    # light = pyrender.DirectionalLight(color=[1, 1, 1], intensity=500.)
+    light = pyrender.PointLight(color=[1, 1, 1], intensity=1000.0)
+    scene.add(light, pose=camera_pose)
+    r = pyrender.OffscreenRenderer(600, 600)
+    color, depth = r.render(scene)
+    return color, depth
+
+
+def plot_reconstruction_comparison(experiment_dirs: List[str], shape_ids: List[str], chckpt: int = 2000, synset_id: str = "02691156", dataset_name: str = "ShapeNetV2", shapenet_dir: str = "/mnt/hdd/ShapeNetCore.v2"):
+    """
+    Plot reconstructions with CD for the same shape reconstructions from different experiments 
+    plus the GT mesh from ShapeNet.
+    """
+    # Plot each shape from different angles.
+    angles = [
+        (-math.pi/2, 0, 0),             # birds-eye-view
+        (-math.pi/4, 3*math.pi/4, 0),   # view from upper-left
+        (0, 3*math.pi/4, 0),    # view from center-left
+    ]
+
+    nrows = len(angles)*len(shape_ids)
+    ncols = len(experiment_dirs)+1
+    fig, ax = plt.subplots(nrows, ncols, figsize=(2*ncols, 2*nrows), dpi=200)
+    # parameter meanings here: https://stackoverflow.com/questions/6541123/improve-subplot-size-spacing-with-many-subplots
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.02, hspace=0.05)
+
+    for r in range(0, len(angles)*len(shape_ids), len(angles)):
+        shape_id = shape_ids[r//len(angles)]
+
+        if r == 0:
+            ax[r, 0].set_title("GT")
+
+        if r % len(angles) == 0:
+            # Add mesh name 
+            left = ax[r, 0].get_position().xmin
+            bottom = ax[r+len(angles)-1, 0].get_position().ymin
+            top = ax[r, 0].get_position().ymax
+            # NOTE: This does not work with fig.tight_layout()
+            fig.text(left-0.01, (top+bottom)/2, shape_id, va="center", ha="right", rotation="vertical")
+        
+        # Plot GT.
+        for i, (alpha, beta, gamma) in enumerate(angles):
+            gt_mesh_path = os.path.join(shapenet_dir, synset_id, shape_id, "models", "model_normalized.obj")
+            gt_mesh = utils.scale_to_unit_sphere(trimesh.load(gt_mesh_path))
+            color, _ = pyrender_helper(gt_mesh, alpha, beta, gamma)
+            ax[r+i, 0].imshow(color)
+            ax[r+i, 0].set_xticks([])
+            ax[r+i, 0].set_yticks([])
+        # Plot other experiments.
+        for c, exp_dir in enumerate(experiment_dirs):
+            c += 1      # first column is GT.
+            if r == 0:
+                title = exp_dir.split(os.sep)[-1]
+                max_title_len = 25
+                title = "\n".join([title[y-max_title_len:y] for y in range(max_title_len, len(title)+max_title_len,max_title_len)])
+                ax[r, c].set_title(title)
+
+            mesh_path = os.path.join(exp_dir, ws.reconstructions_subdir, str(chckpt), ws.reconstruction_meshes_subdir, dataset_name, synset_id, shape_id + ".ply")
+            try:
+                mesh = utils.scale_to_unit_sphere(trimesh.load(mesh_path))
+            except ValueError as e:
+                logging.error(f"File does not exist as path {mesh_path}")
+            cd, cd_all = metrics.compute_metric(gt_mesh, mesh, metric="chamfer")
+            ax[r, c].annotate(f"CD={cd:.6f}", (3, color.shape[0]), va="bottom", ha="left")
+            for i, (alpha, beta, gamma) in enumerate(angles):
+                color, _ = pyrender_helper(mesh, alpha, beta, gamma)
+                ax[r+i, c].imshow(color)
+                ax[r+i, c].set_xticks([])
+                ax[r+i, c].set_yticks([])
+
+    plt.close()
+    return fig
+
+
+def plot_binary_vs_continuous(df: pd.DataFrame, binary: str, continuous: str):
+    """Helper function for plotting the loss against categorical variables of a DataFrame."""
+    fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+    df.plot(x=binary, y=continuous, kind="bar", ax=ax[0])
+    df.groupby(binary).apply(lambda g: g.mean()).plot(y=continuous, kind="bar", ax=ax[1])
