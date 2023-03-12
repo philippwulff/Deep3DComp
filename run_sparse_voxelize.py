d5af5360c1e0cfaaee1b1e014f88a9365c435ac3
Philipp Wulff <philipp.sylt@gmail.com>
2023-03-12 23:49:25 +0100

    added sparse voxelize

--- run_sparse_voxelize.py
+++ run_sparse_voxelize.py
@@ -15,24 +15,17 @@ import math
 import numpy as np
 
 
-def run_sparse_voxelize(input_obj_path: str, voxel_resolution: int, logs: list):
+def run_sparse_voxelize(input_obj_path: str, voxel_resolution: int):
 
     gt_mesh = utils.scale_to_unit_sphere(trimesh.load(input_obj_path))
-
-    example = pd.read_csv("/home/wulff/deepsdf/data/voxelize_until_cd_meshes_CD=0.001/run_voxelize_until_CD_logs.csv").iloc[3]
-    print(example)
-
-    gt_mesh = utils.scale_to_unit_sphere(input_obj_path)
     voxels = mesh_to_sdf.mesh_to_voxels(gt_mesh, voxel_resolution=voxel_resolution, check_result=True, pad=True, sign_method="depth")
-
-    sparse_vox = copy.deepcopy(voxels)
-    voxel_size = 2.0 / (example["voxel_resolution"] - 1)
-    sparse_vox[abs(sparse_vox)>2*math.sqrt(2*voxel_size**2)] = 1
-    verts, faces, normals, values = skimage.measure.marching_cubes(sparse_vox, level=0.0, method="lewiner")
+    voxel_size = 2.0 / (voxel_resolution - 1)
+    voxels[abs(voxels)>2*math.sqrt(2*voxel_size**2)] = 1
+    verts, faces, normals, _ = skimage.measure.marching_cubes(voxels, level=0.0, method="lewiner")
     reconstruction = trimesh.Trimesh(vertices=verts, faces=faces, vertex_normals=normals)
     reconstruction = utils.scale_to_unit_sphere(reconstruction)
     cd, _ = metrics.compute_metric(gt_mesh, reconstruction, metric="chamfer")
-    num_sparse_voxels = (example["voxel_resolution"]+2)**3 - len(sparse_vox[sparse_vox == 1.0])
+    num_sparse_voxels = (voxel_resolution+2)**3 - len(voxels[voxels == 1.0])
 
     return cd, num_sparse_voxels
 
@@ -53,7 +46,6 @@ if __name__ == "__main__":
     utils.add_common_args(arg_parser)
     args = arg_parser.parse_args()
     utils.configure_logging(args)
-    logging.info(f"Using {args.num_threads} cores.")
 
     for vox_log in voxelize_logs:
 
@@ -64,15 +56,15 @@ if __name__ == "__main__":
         log_df["sparse_cd"] = np.nan
 
         try:
-            for i in tqdm(len(log_df)):
-                if log_df.iloc[i]["num_sparse_voxels"] != np.nan:
+            for i in tqdm(range(len(log_df))):
+                if not np.isnan(log_df.iloc[i]["num_sparse_voxels"]):
                     continue
                 cd, num_sparse_voxels = run_sparse_voxelize(
-                    log_df.iloc[i]["input_obj_path"],
-                    log_df.iloc[i]["voxel_resolution"],
+                    log_df.loc[i, "input_obj_path"],
+                    log_df.loc[i, "voxel_resolution"],
                 )
-                log_df.iloc[i]["num_sparse_voxels"] = num_sparse_voxels
-                log_df.iloc[i]["sparse_cd"] = cd
+                log_df.loc[i, "num_sparse_voxels"] = num_sparse_voxels
+                log_df.loc[i, "sparse_cd"] = cd
         except KeyboardInterrupt:
             logging.info("Received KeyboardInterrupt. Cleaning up and exiting.")
         finally:
