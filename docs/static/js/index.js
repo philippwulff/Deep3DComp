11a2c575be89fae87d10318d75bbbc663a0c7797
Philipp Wulff <philipp.sylt@gmail.com>
2023-04-17 18:57:18 +0200

    changed interp frame cnt

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -1,7 +1,7 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+var NUM_INTERP_FRAMES = 29;     
 
 var interp_images = [];
 function preloadInterpolationImages() {
